66a15a02f34ce0fcef860d4b9ebf2c6e8f582c48
Adam Conner-Sax <adam_conner_sax@yahoo.com>
2022-06-23 13:12:44 -0400

    Added test of runner allowing given RandomGen

--- polysemy-RandomFu/test/RandomFuSpec.hs
+++ polysemy-RandomFu/test/RandomFuSpec.hs
@@ -8,10 +8,9 @@ import           Polysemy.RandomFu
 
 import           Test.Hspec
 import           Control.Monad                 as M
-import           Control.Monad.IO.Class         ( liftIO )
 
 import qualified Data.Random                   as R
-
+import qualified System.Random.Mersenne.Pure64 as MR
 import qualified Data.Vector as V
 
 getRandomInts :: Member RandomFu r => Int -> Sem r [Int]
@@ -28,6 +27,10 @@ randomListsDifferent nDraws = do
 
 spec :: Spec
 spec = describe "RandomFu" $ do
-  it "Should produce two distinct sets of psuedo-random Ints." $ do
+  it "Should produce two distinct sets of pseudo-random Ints." $ do
     result <- runM . runRandomIO $ randomListsDifferent 5
+    result `shouldBe` True
+  let mersenneSeed = MR.pureMT 1
+  it "Should produce two distinct sets of pseudo-random ints." $ do
+    result <- runM . runAtomicStatefulRandom mersenneSeed $ randomListsDifferent 5
     result `shouldBe` True
